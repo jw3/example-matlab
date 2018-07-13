76aba9ba194e5817066548c38b09dfc950f6ffe8
John Wass <wassj@ctc.com>
2018-07-13 16:02:18 -0400

    add a lib representing an external dependency that will need to be mocked

---
+++ ext/SomeMccLib.cpp
@@ -0,0 +1,13 @@
+#include "SomeMccLib.h"
+
+bool initializeSomeMccLib() {
+   throw "cant touch this";
+}
+
+void callSomeMccLib(int retc, mwArray retv, mwArray p1, mwArray p2) {
+   throw "cant touch this";
+}
+
+bool terminateSomeMccLib() {
+   throw "cant touch this";
+}
---
+++ ext/SomeMccLib.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#ifndef _MCLCPPCLASS_H_
+#include <mclmcrrt.h>
+#include <mclcppclass.h>
+#endif
+
+bool initializeSomeMccLib();
+void callSomeMccLib(int retc, mwArray retv, mwArray p1, mwArray p2);
+bool terminateSomeMccLib();
